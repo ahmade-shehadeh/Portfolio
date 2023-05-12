801dcc3e72b11de843652271a4ae48b9d2bcda7a
ahmade-shehadeh <ahmade.shehadeh@gmail.com>
2023-05-12 03:52:27 +0300

    END

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const btn = document.querySelector("#btnMenu");
 const menu = document.querySelector("#menu");
-
 btn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
@@ -11,34 +10,43 @@ const nameInput = document.querySelector("#user_name");
 const emailInput = document.querySelector("#user_email");
 const messageInput = document.querySelector("#message");
 
-const Service_ID = "service_4lu4pwo";
+
 
 const Public_Key = "6kDj9mYBfJS_lVA7Z";
 const Private_Key = "X38bma_W8u9_CnC0p_2_b";
-const Template_ID = "template_hnw6igv";
 
-emailjs.init(Public_Key);
 
-contactForm.addEventListener("click", (e) => {
-  e.preventDefault();
+(function () {
+  emailjs.init(Public_Key);
+})();
 
-  sudmitbtn.innerText = "Just A Moment.... ";
-console.log('e.target');
-  const inputFields = {
-    name: nameInput.value,
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  emailjs.send(Service_ID, Template_ID, inputFields).then(
-    () => {
-      sudmitbtn.innerText = "message sent successfully";
-      nameInput.value = "";
-      emailInput.value = "";
-      messageInput.value = "";
-    },
-    (error) => {
-      console.log(error);
-      sudmitbtn.innerText = "something went wrong";
-    }
-  );
-});
+// const inputFields = {
+//   name: nameInput.value,
+//   email: emailInput.value,
+//   message: messageInput.value,
+// };
+document
+        .getElementById("contact_form")
+        .addEventListener("submit", function (event) {
+          event.preventDefault();
+
+          const Service_ID = "service_4lu4pwo";
+          const Template_ID = "template_hnw6igv";
+          
+          const inputFields = {
+  name: nameInput.value,
+  email: emailInput.value,
+  message: messageInput.value,
+};
+          
+          emailjs.send(Service_ID, Template_ID,inputFields).then(
+            (response) => {
+              console.log("SUCCESS!", response.status, response.text);
+              alert("SUCCESS!");
+            },
+            (error) => {
+              console.log("FAILED...", error);
+              alert("FAILED...", error);
+            }
+          );
+        });
--- s.js
+++
@@ -1,7 +0,0 @@
-
-
-//input : word ? number ,string 
-// if word = number convert to string 
-// convert word to arr for aloow to revers it 
-// if word = word.reverse return true else return false
-
